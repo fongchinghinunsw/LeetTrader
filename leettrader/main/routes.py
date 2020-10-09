61dd1eb86032c2ec9e8bf5b6609a44da97ea6e22
Stephen <fongchinghinunsw@gmail.com>
2020-10-09 18:26:41 +1100

    Rearrange templates and create home page template for user

--- leettrader/main/routes.py
+++ leettrader/main/routes.py
@@ -3,5 +3,10 @@ from flask import render_template, Blueprint
 main = Blueprint('main', __name__)
 
 @main.route("/")
-def home():
+def landing():
   return render_template('landing.html')
+
+
+@main.route("/<int:userID>")
+def home(userID):
+  return render_template('home.html')
