07500fda4574f53da1406095d64b0c286a935d3e
Stephen <fongchinghinunsw@gmail.com>
2020-10-09 21:31:00 +1100

    Add missing files from previous commit (autocompletion)

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
 from leettrader import create_app
+from leettrader.models import Stock
 
 app = create_app()
+
+@app.context_processor
+def inject_list_of_stocks():
+  """ Inject the variable `stock` in all the templates """
+  stocks = Stock.query.all()
+  stocks = [stock.name + " " + stock.code for stock in stocks]
+  return dict(stocks=stocks)
 
 if __name__ == '__main__':
   app.run(debug=True)
