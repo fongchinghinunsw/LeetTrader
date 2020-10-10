838b018e96e133147d94d364a4116f4eb628787f
Stephen <fongchinghinunsw@gmail.com>
2020-10-11 00:12:15 +1100

    Add headers to request

--- leettrader/stock/utils.py
+++ leettrader/stock/utils.py
@@ -55,7 +55,7 @@ headers_list = [
 ]
 
 def get_search_result(stock_code):
-  source = requests.get(f'https://finance.yahoo.com/quote/{stock_code}').text
+  source = requests.get(f'https://finance.yahoo.com/quote/{stock_code}', headers=choice(headers_list)).text
   soup = BeautifulSoup(source, 'lxml')
 
   price = soup.find('span', class_="Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)").text
