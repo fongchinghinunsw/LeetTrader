42d1ed9962e0f6ea25d8dff51db0304cfa488cfe
Stephen <fongchinghinunsw@gmail.com>
2020-10-11 00:05:47 +1100

    show current price and price change in search page

--- leettrader/stock/routes.py
+++ leettrader/stock/routes.py
@@ -1,12 +1,15 @@
 from flask import render_template, request, redirect, url_for, Blueprint
 from leettrader.models import Stock
 from leettrader.stock.forms import SearchStockForm
+from leettrader.stock.utils import get_search_result
 
 
 stock = Blueprint('stock', __name__)
 
 @stock.route('/search', methods=['GET', 'POST'])
 def search_stock():
+  """ Route the user to the search page if the stock code is correct
+  """
   stock = SearchStockForm(request.form).stock.data
   code = stock.split()[-1].strip("()")
   if Stock.query.filter_by(code=code).first():
@@ -17,5 +20,15 @@ def search_stock():
 @stock.route('/<string:code>', methods=['GET', 'POST'])
 def search_page(code):
   stock = Stock.query.filter_by(code=code).first()
-  stock = f"{ stock.name } ({ stock.code})"
-  return render_template('search_page.html', stock=stock)
+  print(stock.code)
+  result = get_search_result(stock.code)
+  stock = f"{ stock.name } ({ stock.code })"
+  
+  if float(result['price_change']) == 0:
+    color = "black"
+  elif float(result['price_change']) > 0:
+    color = "green"
+  else:
+    color = "red"
+
+  return render_template('search_page.html', stock=stock, price=result['price'], price_change=result['price_change'], percent_change=result['percent_change'], color=color)
