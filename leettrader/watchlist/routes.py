19e8420b3d81968800516a31bcc96728f63b93e4
LunA <55641527+LunA820@users.noreply.github.com>
2020-10-13 01:14:35 +1100

    Watchlist navigation

--- leettrader/watchlist/routes.py
+++ leettrader/watchlist/routes.py
@@ -7,19 +7,37 @@ from flask_login import current_user, login_required
 
 watchlist = Blueprint('watchlist', __name__)
 
+
 @watchlist.route('/add/<string:code>', methods=['POST'])
 @login_required
 def add_stock_to_watchlist(code):
     add_stocks(current_user, code)
     return {"msg": "Added"}, 200
 
+
 @watchlist.route('/remove/<string:code>', methods=['POST'])
 @login_required
 def remove_stock_from_watchlist(code):
     remove_stocks(current_user, code)
     return {"msg": "Deleted"}, 200
-    
+
+
 @watchlist.route('/get_watchlist', methods=['GET'])
 @login_required
 def get_watchlist():
-    return jsonify(watchlist = get_list(current_user)), 200
+    return jsonify(watchlist=get_list(current_user)), 200
+
+
+@watchlist.route('/get_price/<string:code>', methods=['GET'])
+@login_required
+def get_price(code):
+    return jsonify(stockPrices=get_search_result(code)), 200
+
+
+# @watchlist.route('/get_watchlist_detail', methods=['GET'])
+# @login_required
+# def get_watchlist_detail():
+#     j1 = get_list(current_user)
+
+
+#     return jsonify(watchlist=get_list(current_user)), 200
