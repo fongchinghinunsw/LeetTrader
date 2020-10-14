a7643151ba629860f10cbb7623a8aeec4930c767
LunA <55641527+LunA820@users.noreply.github.com>
2020-10-14 22:22:03 +1100

    Pylint correction

--- leettrader/watchlist/routes.py
+++ leettrader/watchlist/routes.py
@@ -1,6 +1,8 @@
-from flask import render_template, request, redirect, url_for, Blueprint, jsonify
-from leettrader import db
-from leettrader.models import User, Watchlist
+"""
+  Routing of Watchlist
+"""
+
+from flask import Blueprint, jsonify
 from leettrader.stock.utils import get_search_result
 from leettrader.watchlist.utils import add_stocks, remove_stocks, get_list
 from flask_login import current_user, login_required
@@ -11,24 +13,28 @@ watchlist = Blueprint('watchlist', __name__)
 @watchlist.route('/add/<string:code>', methods=['POST'])
 @login_required
 def add_stock_to_watchlist(code):
-  add_stocks(current_user, code)
-  return {"msg": "Added"}, 200
+    ''' Add stock to watchlist'''
+    add_stocks(current_user, code)
+    return {"msg": "Added"}, 200
 
 
 @watchlist.route('/remove/<string:code>', methods=['POST'])
 @login_required
 def remove_stock_from_watchlist(code):
-  remove_stocks(current_user, code)
-  return {"msg": "Deleted"}, 200
+    ''' Remove stock from watchlist'''
+    remove_stocks(current_user, code)
+    return {"msg": "Deleted"}, 200
 
 
 @watchlist.route('/get_watchlist', methods=['GET'])
 @login_required
 def get_watchlist():
-  return jsonify(watchlist=get_list(current_user)), 200
+    ''' Return watchlist info as JSON '''
+    return jsonify(watchlist=get_list(current_user)), 200
 
 
 @watchlist.route('/get_price/<string:code>', methods=['GET'])
 @login_required
 def get_price(code):
-  return jsonify(stockPrices=get_search_result(code)), 200
+    ''' Return price info of stock in watchlist as JSON '''
+    return jsonify(stockPrices=get_search_result(code)), 200
--- leettrader/watchlist/utils.py
+++ leettrader/watchlist/utils.py
@@ -1,50 +1,50 @@
 """
-  utils: contain all helper functions related to the watchlist
+  Backend Utility Functions for Watchlist.
 """
 from datetime import datetime
 from leettrader import db
 from leettrader.models import Watchlist, Stock
 
 
-# Add stock to watchlist in db
 def add_stocks(current_user, code):
-  """
-    add_stocks: add a stock to the user's watchlist
-  """
-  watchlist = Watchlist.query.filter_by(user_id=current_user.get_id()).filter(
-      Watchlist.stocks.any(code=code)).first()
-  if watchlist is None:
-    user_id = current_user.get_id()
-    date_added = datetime.now()
-    stocks = Stock.query.filter_by(code=code).first()
-    watchlist = Watchlist(user_id=user_id,
-                          date_added=date_added,
-                          stocks=[stocks])
-    db.session.add(watchlist)
-    db.session.commit()
+    """ Add stock to Watchlist """
+    # Get Watchlist
+    watchlist = Watchlist.query.filter_by(user_id=current_user.get_id()).filter(
+        Watchlist.stocks.any(code=code)).first()
+
+    # If user does not have a watchlist, make a new one
+    if watchlist is None:
+        user_id = current_user.get_id()
+        date_added = datetime.now()
+        stocks = Stock.query.filter_by(code=code).first()
+        watchlist = Watchlist(user_id=user_id,
+                              date_added=date_added,
+                              stocks=[stocks])
+        db.session.add(watchlist)
+        db.session.commit()
 
 
-# Remove stock from watchlist in db
 def remove_stocks(current_user, code):
-  """
-    remove_stocks: remove a stock to the user's watchlist
-  """
-  watchlist = Watchlist.query.filter_by(user_id=current_user.get_id()).filter(
-      Watchlist.stocks.any(code=code)).first()
-  if watchlist is not None:
-    db.session.delete(watchlist)
-    db.session.commit()
+    """ Remove stock from Watchlist """
+    # Get Watchlist
+    watchlist = Watchlist.query.filter_by(user_id=current_user.get_id()).filter(
+        Watchlist.stocks.any(code=code)).first()
+
+    # If watchlist is empty, delete watchlist in database
+    if watchlist is not None:
+        db.session.delete(watchlist)
+        db.session.commit()
 
 
-def get_list(current_user):  #Flush watchlist on login
-  """
-    get a list of stocks in the user's watchlist in the format
-    [stock_a_code, stock_a_name, stock_b_code, stock_b_name, ...]
-  """
-  codes = []
-  watchlist = db.session.query(Watchlist).filter(
-      Watchlist.user_id == current_user.get_id()).all()
-  for i in watchlist:
-    codes.append(i.stocks[0].code)
-    codes.append(i.stocks[0].name)
-  return codes
+def get_list(current_user):  
+    ''' Return stock code & name of all stock in watchlist '''
+    # Set up ans list and get watchlist of user
+    ans_list = []
+    watchlist = db.session.query(Watchlist).filter(
+        Watchlist.user_id == current_user.get_id()).all()
+
+    # Format of ansList: [code, name, code, name ...]
+    for i in watchlist:
+        ans_list.append(i.stocks[0].code)
+        ans_list.append(i.stocks[0].name)
+    return ans_list
