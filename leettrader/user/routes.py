e6296e43bb1609addcc7cc17b1a10e79ecf67e9f
Oliver Gguo <G1398705330@gmail.com>
2020-10-24 23:47:36 +0800

    refactoring

--- leettrader/user/routes.py
+++ leettrader/user/routes.py
@@ -12,60 +12,14 @@ from leettrader import db, bcrypt, mail
 from flask_login import login_user, logout_user, current_user, login_required
 from flask_mail import Message
 
+from leettrader.user.send_emails import send_confirmation_email, send_reset_password_email, send_delete_account_email
+
 user = Blueprint('user', __name__)
 
 # ugly global variable for now
 new_username = None
 new_email = None
 new_password = None
-
-
-# Use flask-mail to send the email
-def send_confirmation_email(new_user):
-  # get a new token and start timer !
-  token = new_user.get_new_token()
-  msg = Message('Confirmation for new account', sender='leettrader2020@gmail.com', recipients=[new_user.email])
-  msg.body = f'''Dear {new_user.username}, to confirm your email and activate your new account, please click the link below:
-{url_for('user.confirm', token=token, _external=True)}
-
-If you did not create a new account, please just ignore this email.
-
-Cheers,
-LeetTrader Team
-  '''
-  mail.send(msg)
-
-
-# Use flask-mail to send the email
-def send_reset_password_email(user):
-  # get a new token and start timer !
-  token = user.get_new_token()
-  msg = Message('Password reset request', sender='leettrader2020@gmail.com', recipients=[user.email])
-  msg.body = f'''Dear {user.username}, to reset your password, please click the link below:
-{url_for('user.', token=token, _external=True)}
-
-If you did not make the request, please just ignore this email.
-
-Cheers,
-LeetTrader Team
-  '''
-  mail.send(msg)
-
-# Use flask-mail to send the email
-def send_delete_account_email(user):
-  # get a new token and start timer !
-  token = user.get_new_token()
-  msg = Message('Delete the account', sender='leettrader2020@gmail.com', recipients=[user.email])
-  msg.body = f'''Dear {user.username}, to cancel your account, please click the link below:
-{url_for('user.delete_account_token', token=token, _external=True)}
-Please be aware that all your account information and data will be deleted and cannot be retrieved.
-
-If you did not make the request, please just ignore this email.
-
-Cheers,
-LeetTrader Team
-  '''
-  mail.send(msg)
 
 
 @user.route("/home")
@@ -161,7 +115,6 @@ def logout():
   
   logout_user()
   return redirect(url_for('main.landing'))
-
 
 
 @user.route("/resetPassword", methods=['GET', 'POST'])
