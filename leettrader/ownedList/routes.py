0e97f724441ea2b1f1cf1fa11bd0ee3ad025438b
LunA <55641527+LunA820@users.noreply.github.com>
2020-10-28 02:39:35 +1100

    pylint fix

--- leettrader/ownedList/routes.py
+++ leettrader/ownedList/routes.py
@@ -3,13 +3,11 @@
 """
 
 from flask import Blueprint, jsonify
-from leettrader.stock.utils import get_search_result
 from flask_login import current_user, login_required
-
 from leettrader import db
-from leettrader.stock.utils import get_search_result
-from leettrader.models import Watchlist, Stock, OwnStock
+from leettrader.models import Stock, OwnStock
 from leettrader.formatter import format_owned_info, color_span
+from leettrader.stock.utils import get_search_result
 
 
 ownedList = Blueprint('ownedList', __name__)
@@ -17,51 +15,49 @@ ownedList = Blueprint('ownedList', __name__)
 
 @ownedList.route('/get_ownedList', methods=['GET'])
 @login_required
-def get_ownedList():
+def get_owned_list():
   ''' Return list of owned stocks in JSON format '''
   ans = get_ownedlist_from_db()
   return jsonify(ownedList=ans), 200
 
 
 
-
 def get_ownedlist_from_db():
   ''' Return list of owned stocks '''
   ans_list = []
-  tot_PL = 0
-  
+  tot_pl = 0
+
   # Access database, get list of owned stocks from user id
-  ol = db.session.query(OwnStock).filter(
+  own_list = db.session.query(OwnStock).filter(
       OwnStock.user_id == current_user.get_id()).all()
 
   # For each owned stock, create a HTML formatted string
-  for item in ol:
+  for item in own_list:
     # Get information of a owned stock
-    stockInfo = getStockInfo(item.stock_id)
-    name = stockInfo[0]
-    market = stockInfo[1] * int(item.unit)
-    currency = stockInfo[2]
+    stock_info = get_stock_info(item.stock_id)
+    name = stock_info[0]
+    market = stock_info[1] * int(item.unit)
+    currency = stock_info[2]
     purchase = float(item.total_purchase_price)
 
     # Calculate P/L
-    pl = round(market-purchase, 2)
-    tot_PL += pl
+    profit = round(market-purchase, 2)
+    tot_pl += profit
 
-    # Format Info & P/L into <div> and <span>
-    ownedListItem = format_owned_info(name, item.unit, currency, market, purchase, pl)
-    ans_list.append(ownedListItem)
-    tot_PL_html = color_span(tot_PL)
-  
-  return [ans_list, tot_PL_html]
+    # Format Info into <div>
+    item = format_owned_info(name, item.unit, currency, market, purchase, profit)
+    ans_list.append(item)
+
+  return [ans_list, color_span(tot_pl)]
 
 
-def getStockInfo(stockID):
+def get_stock_info(stock_id):
   ''' Return stock information of a stock '''
   target = db.session.query(Stock).filter(
-      Stock.id == int(stockID)).first()
-  
+      Stock.id == int(stock_id)).first()
+
   info = get_search_result(target.code)
-  marketPrice = float(info['price'])
+  worth = float(info['price'])
   currency = info['currency']
-  
-  return [target.name, marketPrice, currency]
+
+  return [target.name, worth, currency]
