3fe6946a5b468149e07e36303517bab9a4769227
Stephen <fongchinghinunsw@gmail.com>
2020-10-27 16:24:52 +1100

    successfully run thread

--- leettrader/user/routes.py
+++ leettrader/user/routes.py
@@ -1,7 +1,7 @@
 """
   Routing of Account Mangement, Simul-Buy and Sell
 """
-from flask import render_template, url_for, flash, redirect, Blueprint, request
+from flask import render_template, url_for, flash, redirect, Blueprint, request, current_app, has_app_context
 from leettrader.user.forms import LoginForm, RegisterForm, OrderForm, CheckoutForm, ReminderForm
 from leettrader.user.utils import add_and_start_reminder
 from leettrader.stock.utils import get_search_result
@@ -210,7 +210,6 @@ def add_reminder():
     if reminder_form.alert_price.data:
       stock_obj = Stock.query.filter_by(code=code).first()
       reminder = Reminder(user_id=current_user.get_id(), stock_id=stock_obj.code, orig_price=get_search_result(stock_obj.code)['price'], target_price=reminder_form.alert_price.data)
-      print(reminder)
       add_and_start_reminder(reminder)
       return redirect(url_for('stock.search_page', code=code))
       
--- leettrader/user/utils.py
+++ leettrader/user/utils.py
@@ -1,7 +1,9 @@
 from threading import Condition, Thread
 from time import sleep
 
+from flask import has_app_context
 from leettrader.stock.utils import get_search_result
+from leettrader.models import User
 
 def add_and_start_reminder(reminder):
   thread = Thread(name="ReminderHandler", target=reminder_handler, args=[reminder])
@@ -10,21 +12,27 @@ def add_and_start_reminder(reminder):
 
 def reminder_handler(reminder):
   stock_code = reminder.stock_id
-  orig_price = reminder.orig_price
-  target_price = reminder.target_price
-  if orig_price < target_price:
-    while True:  
-      current_price = get_search_result(stock_code)
-      if current_price >= target_price:
-        print("Hit the price, thread stopped")
-        break
-      sleep(5)
-      print("sleep for 5 seconds...")
-  else:
-    while True:
-      current_price = get_search_result(stock_code)
-      if current_price <= target_price:
-        print("Hit the price, thread stopped")
-        break
-      sleep(5)
-      print("sleep for 5 seconds...")
+  orig_price = float(reminder.orig_price)
+  target_price = float(reminder.target_price)
+
+  from leettrader import create_app
+  app = create_app()
+
+  with app.app_context():
+    print(User.query.all())
+    if orig_price < target_price:
+      while True:  
+        current_price = float(get_search_result(stock_code)['price'])
+        if current_price >= target_price:
+          print("Hit the price, thread stopped")
+          break
+        sleep(5)
+        print("sleep for 5 seconds...")
+    else:
+      while True:
+        current_price = float(get_search_result(stock_code)['price'])
+        if current_price <= target_price:
+          print("Hit the price, thread stopped")
+          break
+        sleep(5)
+        print("sleep for 5 seconds...")
