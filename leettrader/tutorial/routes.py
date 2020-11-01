6e82b3d149b440ad295ef5e375d0315ecb9a5825
Natural-Twenty <lethaldragon@hotmail.com>
2020-11-01 23:54:30 +1100

    renamed html files and corresponding variables

--- leettrader/tutorial/routes.py
+++ leettrader/tutorial/routes.py
@@ -10,30 +10,32 @@ tutorial = Blueprint('tutorial', __name__)
 @tutorial.route("/usage_guide", methods=['GET'])
 def usage_guide():
   ''' Complete usage guide '''
+  # Get content from the JSON file which contains the tutorial.
   guides = json.loads(open('leettrader/tutorial/intents.json').read())
   usage_guide_str = ""
   
   # Title
   usage_guide_str += "<h1> Learn how to use LeetTrader </h1> <br>"
   # Body
-  for tag in guides['guides']:
+  for intent in guides['intents']:
     # Context 1 is for tutorial on how to use LeetTrader
-    if guides['context'] == 1:
-      usage_guide_str += "<h3>" + tag + "</h3>" + "<br>" + "<p>" + tag['response'] + "</p>" + "<br>"
+    if intent['context'] == 1:
+      usage_guide_str += "<h3>" + intent['tag'] + "</h3>" + "<br>" + "<p>" + intent['responses'][0] + "</p>" + "<br>"
 
-  return render_template('tutorial.html', usage_guide_str=usage_guide_str)
+  return render_template('usage_guide.html', usage_guide_str=usage_guide_str)
 
 
-@tutorial.route("/help", methods=['GET'])
+@tutorial.route("/tutorial", methods=['GET'])
 def help():
   ''' Learning knowledge about trading stocks '''
+  # Get content from the JSON file which contains the tutorial.
   trading = json.loads(open('leettrader/tutorial/intents.json').read())
   trading_help = ""
   # Title
   trading_help += "<h1> Learn trading terminology </h1> <br>"
   # Body
-  for tag in trading['trading']:
+  for intent in trading['intents']:
     # Context 2 is for learning trading terminology.
-    if trading['context'] == 2:
-      trading_help += "<h3>" + tag + "</h3>" + "<br>" + "<p>" + tag['response'] + "</p>" + "<br>"
-  return render_template('help.html', trading_help=trading_help)
+    if intent['context'] == 2:
+      trading_help += "<h3>" + intent['tag'] + "</h3>" + "<br>" + "<p>" + intent['responses'][0] + "</p>" + "<br>"
+  return render_template('tutorial.html', trading_help=trading_help)
