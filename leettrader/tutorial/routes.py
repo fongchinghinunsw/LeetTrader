41b355fd314b514ef59f0d6b8bf12ce657400e78
Natural-Twenty <lethaldragon@hotmail.com>
2020-11-01 15:47:28 +1100

    added route for help page

--- leettrader/tutorial/routes.py
+++ leettrader/tutorial/routes.py
@@ -5,29 +5,30 @@
 import json
 from flask import render_template, Blueprint
 
-guide = Blueprint('guide', __name__)
+tutorial = Blueprint('tutorial', __name__)
 
 @tutorial.route("/tutorial", methods=['GET'])
-def tutorial():
+def usage_guide():
   ''' Complete usage guide '''
   guides = json.loads(open('leettrader/tutorial/tutorial.json').read())
-  usage_guide = ""
-  ''' Title '''
-  usage_guide += "<h1> Learn how to use LeetTrader </h1> <br>"
-  ''' Body '''
+  usage_guide_str = ""
+  # Title
+  usage_guide_str += "<h1> Learn how to use LeetTrader </h1> <br>"
+  # Body
   for tag in guides['guides']:
-    usage_guide += "<h3>" + tag + "</h3>" + "<br>" + "<p>" + tag['response'] + "</p>" + "<br>"
+    usage_guide_str += "<h3>" + tag + "</h3>" + "<br>" + "<p>" + tag['response'] + "</p>" + "<br>"
 
-  return render_template('tutorial.html', usage_guide=usage_guide)
+  return render_template('tutorial.html', usage_guide_str=usage_guide_str)
 
 
 @tutorial.route("/learntrading")
 def help():
   ''' Learning knowledge about trading stocks '''
-  guides = json.loads(open('leettrader/tutorial/trading.json').read())
+  trading = json.loads(open('leettrader/tutorial/trading.json').read())
   trading_help = ""
-  ''' Title '''
+  # Title
   trading_help += "<h1> Learn trading terminology </h1> <br>"
-  for tag in guides['guides']:
+  # Body
+  for tag in trading['trading']:
     trading_help += "<h3>" + tag + "</h3>" + "<br>" + "<p>" + tag['response'] + "</p>" + "<br>"
   return render_template('help.html', trading_help=trading_help)
