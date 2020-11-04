511085c9959592c13954565e84b434ead3afd5a0
Stephen <fongchinghinunsw@gmail.com>
2020-11-04 12:35:17 +1100

    fix blueprint user bug

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,17 +5,13 @@ import tempfile
 
 import pytest
 from leettrader import create_app
-from run import app
 from utils import init_db
 
 
 @pytest.fixture
 def client():
     """ This client fixture will be called by each individual test """
-    # returns a low-level file handle and a random file name
-    db_fd, app.config['DATABASE'] = tempfile.mkstemp()
-    # disable error catching during request handling, so that you get
-    # better error reports when performing test requests against the application.
+    app = create_app()
     app.config['TESTING'] = True
 
     with app.test_client() as client:
@@ -25,5 +21,6 @@ def client():
         print("yield the client")
         yield client
 
-    os.close(db_fd)
-    os.unlink(app.config['DATABASE'])
+@pytest.fixture
+def app():
+  yield app
---
+++ tests/test_stock.py
@@ -0,0 +1,5 @@
+from leettrader.stock.utils import get_currency_by_stock_code
+
+def test_get_currency_by_stock_code(app):
+  with app.app_context():
+    assert get_currency_by_stock_code("ABA.NZ") == "NZD"
