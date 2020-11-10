b6df376cfd1903f8e0777b863a3b5c8631a68438
fongchinghinunsw <fongchinghinunsw@gmail.com>
2020-11-10 19:15:09 +1100

    Train model when run execute.sh

--- execute.sh
+++ execute.sh
@@ -4,4 +4,5 @@ virtualenv --seeder=pip /var/tmp/leettrader/venv
 source /var/tmp/leettrader/venv/bin/activate
 pip install -r requirements.txt
 python utils.py
+python leettrader/tutorial/train.py
 python run.py
