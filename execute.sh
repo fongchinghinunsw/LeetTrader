d9614a793f143836679d801367c8fd3adcc12725
fongchinghinunsw <fongchinghinunsw@gmail.com>
2020-11-12 11:24:27 +1100

    Modify execute.sh

--- execute.sh
+++ execute.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # IMPORTANT: This script is only for the tutor/student to run the application in the vlab.
+pip3 install virtualenv
+export PATH=$PATH:~/.local/bin
 virtualenv --seeder=pip /var/tmp/leettrader/venv
 source /var/tmp/leettrader/venv/bin/activate
 pip install -r requirements.txt
