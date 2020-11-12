b1824bc28d67ee06e221d4536535f8073a7bd503
Stephen <fongchinghinunsw@gmail.com>
2020-11-12 11:59:32 +1100

    modify port

--- leettrader/config.py
+++ leettrader/config.py
@@ -5,6 +5,8 @@ import os
 
 
 class Config:
+  SERVER_NAME = "localhost:6969"
+
   SECRET_KEY = '7b0dff182c1a883a7c12855dcc6f411d'
   SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
   SQLALCHEMY_TRACK_MODIFICATIONS = False
